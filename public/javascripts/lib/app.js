2c6433b5773c5e60ef06cee75858a4a996cb26b4
undrizzle <undrizzle@gmail.com>
2015-03-19 16:18:04 +0800

    add grumble

---
+++ public/javascripts/lib/app.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('.nav-recommend').grumble(
+        {
+            text: '￥1500',
+            sizeRange: 50,
+            angle: 90,
+            distance: 45,
+            showAfter: 1000
+          //  hideAfter: 2000
+        }
+    );
+});
