a1cd2708de29c3084884ae4d013b2edce0e8069e
Undrizzle <undrizzle@gmail.com>
2015-05-10 21:40:06 +0800

    add message valid

--- client/src/app/components/messagevaild.js
+++ client/src/app/components/messagevaild.js
@@ -3,6 +3,14 @@ var mui = require('material-ui');
 var RaisedButton = mui.RaisedButton;
 
 module.exports = MessageVaild = React.createClass({
+    MobileValid: function() {
+        var mobile = $('.num-input').val().trim();
+        var validReg = /^13[0-9]{9}$|14[0-9]{9}|15[0-9]{9}$|18[0-9]{9}$/;
+        if(validReg.test(mobile))
+            alert('格式正确');
+        else
+            alert('格式不正确');
+    },
     render: function() {
         return (
             <div className="profile-block msg-valid" id="valid">
@@ -12,8 +20,8 @@ module.exports = MessageVaild = React.createClass({
                 <div className="content">
                     <div className="">
                         <label className="control-label">手机</label>
-                        <input className="num-input" />
-                        <div className="input-btn">
+                        <input className="num-input" type="number" />
+                        <div className="input-btn" onClick={this.MobileValid}>
                             <RaisedButton label="免费发送验证短信" primary />
                         </div>
                     </div>
--- client/src/app/components/wechat.js
+++ client/src/app/components/wechat.js
@@ -1,13 +1,16 @@
 var React = require('react');
 var mui = require('material-ui');
+var MessageBox = require('./messagebox.js');
 
 module.exports = WeChat = React.createClass({
+    showMessageBox: function() {
+        document.getElementById('MessageBox');
+    },
     render: function() {
         return (
-            <div className="mechat-pcbin">
+            <div className="wechat-pcbin" onclick={this.showMessageBox}>
                 <span id="MECHAT-BTN">寻求帮助</span>
             </div>
-
         )
     }
 });
