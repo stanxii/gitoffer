f54d49923f5984527166785cc2da08cd9a0c9622
Stan Xi <stan.xii@gmail.com>
2015-10-29 06:39:09 +0000

    new db

--- cheeriio/indeed.real.js
+++ cheeriio/indeed.real.js
@@ -21,8 +21,8 @@ app.get('/', function (req, res, next) {
   //var url = 'http://www.indeed.com/l-Anchorage,-AK-jobs.html';
 
   var options = {
-    limit: 20000,
-    skip: 21291 
+    limit: 30000,
+    skip: 22541 
   }
   //get all origins counts nums. 166587
   IndeedsDAO.getOriginsCount().then((count) => {
