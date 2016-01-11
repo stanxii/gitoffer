d92c42fe0610702e7f7fc549694d771f877bd802
Stan Xi <stan.xii@gmail.com>
2016-01-11 15:38:59 +0800

    add api 192.168.31.209:9000/jobs/onepage/0/20  /onepage/:from/size one page mock api

--- greatejob-api/app.js
+++ greatejob-api/app.js
@@ -1,12 +1,15 @@
-var jobs = require('./jobs');
+var jobRouter = require('./jobs');
 var birds = require('./birds');
 var express    = require('express');
+var bodyParser = require('body-parser');
 
 var port     = process.env.PORT || 9000; // set our port
 
 var app  = express();
+app.use(bodyParser.json());
 
-app.use('/jobs', jobs);
+
+app.use('/jobs', jobRouter);
 app.use('/birds', birds);
 
 app.listen(port);
--- greatejob-api/jobs.js
+++ greatejob-api/jobs.js
@@ -1,18 +1,30 @@
 var express = require('express');
-var router = express.Router();
+var jobRouter = express.Router();
 
 // middleware that is specific to this router
-router.use(function timeLog(req, res, next) {
+jobRouter.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
   next();
 });
 // define the home page route
-router.get('/', function(req, res) {
+jobRouter.get('/', function(req, res) {
   res.send('Jobs home page');
 });
 // define the about route
-router.get('/about', function(req, res) {
+jobRouter.get('/about', function(req, res) {
   res.send('About jobs');
 });
 
-module.exports = router;
+ const initJobs = [
+  {id: 1, category: 'it', title: 'ios developer', company: 'Calsoft Labs', location: 'Sunnyvale, CA 94085', description: '3 years of working experience in iOS development. Design and build advanced applications for the iOS platform...', pubdate: '3 days ago', last_update:'2015-10-23T06:21:32.906Z', detail_link:'http://maxpoint.com/us/digital-advertising-company/online-advertising-careers/online-advertising-jobs?gnk=job&gni=8a7882474fb9d3be014fbd14812d13e0&gns=Simply+Hired', salary: 0, fulltime: true},
+  {id: 2, category: '', title: '', company: '', location: '', description:'',  pubdate:'', last_update:'', detail_link:'', salary: 0, fulltime: true},
+  
+];
+// define the mock jobs json data route
+jobRouter.get('/onepage/:from?/:size?', function(req, res) {
+  var from = req.params.from;
+  var size = req.params.size;
+  res.send(initJobs);
+});
+
+module.exports = jobRouter;
