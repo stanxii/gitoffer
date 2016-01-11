46e9b7c9df578f8e14f61568df2955db8052bfb5
Stan Xi <stan.xii@gmail.com>
2016-01-11 13:28:18 +0800

    parrell two express.Router /birds and /jobs all together works fine two route stuff

--- greatejob-api/app.js
+++ greatejob-api/app.js
@@ -1,8 +1,13 @@
+var jobs = require('./jobs');
 var birds = require('./birds');
 var express    = require('express');
 
+var port     = process.env.PORT || 9000; // set our port
+
 var app  = express();
 
+app.use('/jobs', jobs);
 app.use('/birds', birds);
 
-app.listen(9000);
+app.listen(port);
+console.log('Api Server... listening  on port ' + port);
