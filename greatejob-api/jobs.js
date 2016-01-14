cefde501449bf375acc46240f4d0461c77beecdf
Stan Xi <stan.xii@gmail.com>
2016-01-14 20:55:59 +0800

    get json api response.entities.jobs data and map to action but state -> stone.prop.jobs not maps have data array to object transform err

--- greatejob-api/jobs.js
+++ greatejob-api/jobs.js
@@ -30,15 +30,15 @@ jobRouter.get('/about', function(req, res) {
   {id: 12, category: 'it', title: 'ios developer', company: 'Calsoft Labs', location: 'Sunnyvale, CA 94085', description: '3 years of working experience in iOS development. Design and build advanced applications for the iOS platform...', pubdate: '3 days ago', last_update:'2015-10-23T06:21:32.906Z', detail_link:'http://maxpoint.com/us/digital-advertising-company/online-advertising-careers/online-advertising-jobs?gnk=job&gni=8a7882474fb9d3be014fbd14812d13e0&gns=Simply+Hired', salary: 0, fulltime: true},
   {id: 13, category: 'it', title: 'ios developer', company: 'Calsoft Labs', location: 'Sunnyvale, CA 94085', description: '3 years of working experience in iOS development. Design and build advanced applications for the iOS platform...', pubdate: '3 days ago', last_update:'2015-10-23T06:21:32.906Z', detail_link:'http://maxpoint.com/us/digital-advertising-company/online-advertising-careers/online-advertising-jobs?gnk=job&gni=8a7882474fb9d3be014fbd14812d13e0&gns=Simply+Hired', salary: 0, fulltime: true, icon: 'http://image.cardvrworld.com/icon/0/6551.ico'},
   {id: 14, category: 'it', title: 'ios developer', company: 'Calsoft Labs', location: 'Sunnyvale, CA 94085', description: '3 years of working experience in iOS development. Design and build advanced applications for the iOS platform...', pubdate: '3 days ago', last_update:'2015-10-23T06:21:32.906Z', detail_link:'http://maxpoint.com/us/digital-advertising-company/online-advertising-careers/online-advertising-jobs?gnk=job&gni=8a7882474fb9d3be014fbd14812d13e0&gns=Simply+Hired', salary: 0, fulltime: true, icon: 'http://image.cardvrworld.com/icon/0/6551.ico'},
-  {id: 15, category: 'it', title: 'ios developer', company: 'Calsoft Labs', location: 'Sunnyvale, CA 94085', description: '3 years of working experience in iOS development. Design and build advanced applications for the iOS platform...', pubdate: '3 days ago', last_update:'2015-10-23T06:21:32.906Z', detail_link:'http://maxpoint.com/us/digital-advertising-company/online-advertising-careers/online-advertising-jobs?gnk=job&gni=8a7882474fb9d3be014fbd14812d13e0&gns=Simply+Hired', salary: 0, fulltime: true, icon: 'http://image.cardvrworld.com/icon/0/6551.ico'},
+  {id: 15, category: 'it', title: 'ios developer', company: 'Calsoft Labs', location: 'Sunnyvale, CA 94085', description: '3 years of working experience in iOS development. Design and build advanced applications for the iOS platform...', pubdate: '3 days ago', last_update:'2015-10-23T06:21:32.906Z', detail_link:'http://maxpoint.com/us/digital-advertising-company/online-advertising-careers/online-advertising-jobs?gnk=job&gni=8a7882474fb9d3be014fbd14812d13e0&gns=Simply+Hired', salary: 0, fulltime: true, icon: 'http://image.cardvrworld.com/icon/0/6551.ico'}
   
-];
+ ];
 // define the mock jobs json data route
 jobRouter.get('/onepage/:ifrom?/:isize?', function(req, res) {
   var ifrom = req.params.ifrom;
   var isize = req.params.isize;
   console.log('ifrom='+ifrom + 'isize='+isize);
-  res.send(initJobs);
+  res.json(initJobs);
 });
 
 module.exports = jobRouter;
